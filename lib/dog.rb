954874dcc53ebb4a9f8aab60ab072f2e7c46fe76
conrad_buys <conradbuys11@gmail.com>
2020-08-25 15:07:31 -0400

    pushing assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
-#testing
+class Dog
+    def initialize(name, breed = "Mutt")
+        @name = name
+        @breed = breed
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+end
